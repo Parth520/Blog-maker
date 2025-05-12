1ecd889a2334016018051a8393d71dad3ca02766
Parth520 <parthbhai200309@gmail.com>
2025-05-12 10:11:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import bodyParser from "body-parser";
 const __dirname = dirname(fileURLToPath(
     import.meta.url));
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
